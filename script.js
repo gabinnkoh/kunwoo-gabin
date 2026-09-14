426a9874f932960a06c9098aaafedb020aeef170
gabin koh <boriegabin@gmail.com>
2026-09-14 22:24:11 +0900

    update img

--- script.js
+++ script.js
@@ -429,7 +429,7 @@ const galleryImages = [
   "images/gallery07.jpg?v=20260905-15",
   "images/ending.jpg?v=20260905-15",
   "images/gallery09.jpg?v=20260905-15",
-  "images/gallery10.jpg?v=20260905-15",
+  "images/gallery10.jpg?v=20260914-1",
   "images/gallery08.jpg?v=20260905-15",
   "images/gallery12.jpg?v=20260905-15"
 ];
